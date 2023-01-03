74150a23c22410429db6bec48509185104e282d0
mwojcickaa <martynawojcickaaa@gmail.com>
2023-01-03 21:44:51 +0100

    Display as many products as the user enters in the input

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,21 @@
-const div = document.createElement("div")
-fetch('https://dummyjson.com/products?limit=6')
-    .then(res => res.json())
-    .then(data => {
-        for (let i = 0; i < 6; i++) {
-            const div = document.createElement("div")
-            div.className = "products"
-            const image = document.createElement("img")
-            const paragraph = document.createElement("p")
-            image.src = data.products[i].images[0];
-            paragraph.innerHTML = data.products[i].title;
-            div.appendChild(image);
-            div.appendChild(paragraph)
-            document.getElementById("shop-gallery").appendChild(div)
-        }
-        const button = document.getElementById("button")
-        button.addEventListener("click", function () {
-            const input = document.getElementById("input").value
-            console.log(input)
-            if (input == 1) {
-                console.log("1221")
+const button = document.getElementById("button")
+button.addEventListener("click", function () {
+    const inputValue = document.getElementById("input").value
+    const link = 'https://dummyjson.com/products?limit'
+    const text = link.replace('https://dummyjson.com/products?limit', 'https://dummyjson.com/products?limit=' + inputValue)
+    fetch(text)
+        .then(res => res.json())
+        .then(data => {
+            for (let i = 0; i < 31; i++) {
+                const div = document.createElement("div")
+                div.className = "products"
+                const image = document.createElement("img")
+                const paragraph = document.createElement("p")
+                image.src = data.products[i].images[0];
+                paragraph.innerHTML = data.products[i].title;
+                div.appendChild(image);
+                div.appendChild(paragraph)
+                document.getElementById("shop-gallery").appendChild(div)
             }
         })
-    })
-
-
-
+})
