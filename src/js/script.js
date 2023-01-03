fa5cffc08f811048c8d3b35da385f4cdd1c03b0f
mwojcickaa <martynawojcickaaa@gmail.com>
2023-01-03 23:33:36 +0100

    Fix loop and create a new price element

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,23 @@
 const button = document.getElementById("button")
 button.addEventListener("click", function () {
+    document.getElementById("shop-gallery").innerHTML = "";
     const inputValue = document.getElementById("input").value
-    const link = 'https://dummyjson.com/products?limit'
-    const text = link.replace('https://dummyjson.com/products?limit', 'https://dummyjson.com/products?limit=' + inputValue)
-    fetch(text)
+    const link = 'https://dummyjson.com/products?limit=' + inputValue
+    fetch(link)
         .then(res => res.json())
         .then(data => {
-            for (let i = 0; i < 31; i++) {
+            for (let i = 0; i < inputValue; i++) {
                 const div = document.createElement("div")
                 div.className = "products"
                 const image = document.createElement("img")
                 const paragraph = document.createElement("p")
-                image.src = data.products[i].images[0];
-                paragraph.innerHTML = data.products[i].title;
-                div.appendChild(image);
+                const price = document.createElement("h4")
+                image.src = data.products[i].images[0]
+                paragraph.innerHTML = data.products[i].title
+                price.innerHTML = data.products[i].price + ".00" + " " + "$"
+                div.appendChild(image)
                 div.appendChild(paragraph)
+                div.appendChild(price)
                 document.getElementById("shop-gallery").appendChild(div)
             }
         })
